d95bef38cd21239bf5ede2fe5b94d5bd381cffbb
Humberto Barrientos <Hawksmoor@gmail.com>
2017-10-20 14:32:09 +0200

    fix linter

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const webpack = require('webpack');
-const config = require('../webpack.config');
+const express = require("express");
+const webpackMiddleware = require("webpack-dev-middleware");
+const webpackHotMiddleware = require("webpack-hot-middleware");
+const webpack = require("webpack");
+const config = require("../webpack.config");
 
-const isDev = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV !== "production";
 const port = isDev ? 8555 : process.env.PORT;
 const app = express();
 
@@ -12,7 +12,7 @@ if (isDev) {
   const compiler = webpack(config);
   const middleware = webpackMiddleware(compiler, {
     publicPath: config.output.publicPath,
-    contentBase: 'src',
+    contentBase: "src/app",
     stats: {
       colors: true,
       hash: false,
@@ -24,21 +24,27 @@ if (isDev) {
   });
   app.use(middleware);
   app.use(webpackHotMiddleware(compiler));
-  app.get('*', function response(req, res) {
-    console.log(req, res)
-    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'build/index.html')));
+  app.get("*", function response(req, res) {
+    console.log(req, res);
+    res.write(
+      middleware.fileSystem.readFileSync(
+        path.join(__dirname, "/app/index.html")
+      )
+    );
     res.end();
   });
 } else {
-  app.use(express.static(__dirname + '/dist'));
-  app.get('*', function response(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  app.use(express.static(__dirname + "/build"));
+  app.get("*", function response(req, res) {
+    res.sendFile(path.join(__dirname, "/build/index.html"));
   });
 }
 
-app.listen(port, (err) => {
+app.listen(port, err => {
   if (err) {
     console.warn(err);
   }
-  console.info(`==> 🌎 Listening on port ${port}. Open up http://0.0.0.0:${port}/ in your browser.`)
+  console.info(
+    `==> 🌎 Listening on port ${port}. Open up http://0.0.0.0:${port}/ in your browser.`
+  );
 });
